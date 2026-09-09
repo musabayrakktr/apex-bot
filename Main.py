bd467191452522b67c837658cf5747b496519965
musabayrakktr <musabayrakktr@gmail.com>
2026-09-10 00:11:49 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,19 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot aktif çalışıyor!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+Thread(target=run).start()
+
 import ccxt
 import pandas as pd
 import requests

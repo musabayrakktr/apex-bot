93d86621b99488fd5653eb7d90ce542598be5ce7
musabayrakktr <musabayrakktr@gmail.com>
2026-09-12 16:03:28 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+TELEGRAM_TOKEN = "8851186730:AAH5HyZBXPGwiuitUYagaq1dgcwte_fl34M"
+CHAT_ID = "8982017587"
+
+OKX_API_KEY = os.environ.get("OKX_API_KEY", "")
+OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY", "")
+OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE", "")
+
+# Geçmiş işlem hafızası
+TRADE_HISTORY = [
+    {"parite": "BTC/USDT", "islem": "KÂR 🟢", "tutar": "+1.25 USDT", "oran": "%1.2", "zaman": "12.09.2026 - 14:10"},
+]

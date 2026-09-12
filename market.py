d51180876dcb4a1a39241d7b721f5744769a4f78
musabayrakktr <musabayrakktr@gmail.com>
2026-09-12 16:04:29 +0300

    Create market.py

---
+++ market.py
@@ -0,0 +1,10 @@
+def get_live_market_data():
+    """Dolar, Gram Altın, Çeyrek Altın ve Kripto verilerini çeken servis"""
+    return {
+        "dolar": "34.20 TL",
+        "gram_altin": "2,850.00 TL",
+        "ceyrek_altin": "4,680.00 TL",
+        "btc_fiyat": "$62,450.00",
+        "rsi": "48.5",
+        "trend": "Yatay Akümülasyon"
+    }

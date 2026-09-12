99dd2002cdd2f11db5b1ff81555de40b32d608d5
musabayrakktr <musabayrakktr@gmail.com>
2026-09-12 21:54:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ OKX_API_KEY = os.environ.get("OKX_API_KEY", "")
 OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY", "")
 OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE", "")
 
-# Geçmiş işlem hafızası
 TRADE_HISTORY = [
     {"parite": "BTC/USDT", "islem": "KÂR 🟢", "tutar": "+1.25 USDT", "oran": "%1.2", "zaman": "12.09.2026 - 14:10"},
 ]

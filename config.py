892ad2df4207ff1e7c18672661ee14f01d747a30
musabayrakktr <musabayrakktr@gmail.com>
2026-09-13 15:13:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 import os
 
-# Tüm hassas bilgiler güvenli bir şekilde Render Environment'tan alınır
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 OKX_API_KEY = os.environ.get("OKX_API_KEY", "")
 OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY", "")
 OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE", "")
+
+# Senin Telegram ID'n güvenlik için ekleniyor
+ADMIN_ID = 8982017587
 
 SYMBOLS = ["SOL/USDT", "BTC/USDT", "ETH/USDT"]

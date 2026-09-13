45c22a4db569bb39c94b54af4617370edf793a8f
musabayrakktr <musabayrakktr@gmail.com>
2026-09-13 17:00:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("8992793254:AAG1QpS7CjO8e3H-2zQp9Benf1mi_-J4HVk")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 8982017587))
 
 OKX_API_KEY = os.environ.get("OKX_API_KEY")

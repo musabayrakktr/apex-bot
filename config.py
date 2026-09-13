0c2cbd0fffff131676926756cffbed28cf51f2c0
musabayrakktr <musabayrakktr@gmail.com>
2026-09-13 16:32:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,4 @@ OKX_API_KEY = os.environ.get("OKX_API_KEY", "")
 OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY", "")
 OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE", "")
 
-# Senin Telegram ID'n güvenlik için ekleniyor
-ADMIN_ID = 8982017587
-
-SYMBOLS = ["SOL/USDT", "BTC/USDT", "ETH/USDT"]
+ADMIN_ID = 8982017587  # Senin Telegram ID'n

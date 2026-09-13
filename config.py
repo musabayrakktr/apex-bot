afcce964977fa4e0e4a68ed703103d39b217d2ae
musabayrakktr <musabayrakktr@gmail.com>
2026-09-13 17:20:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", 8982017587))
-
-OKX_API_KEY = os.environ.get("OKX_API_KEY")
-OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY")
-OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE")

d9b1078e2a152c0d18da75bf751e1390d0745bd2
musabayrakktr <musabayrakktr@gmail.com>
2026-09-13 16:54:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-OKX_API_KEY = os.environ.get("OKX_API_KEY", "")
-OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY", "")
-OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE", "")
+TELEGRAM_TOKEN = os.environ.get("8992793254:AAG1QpS7CjO8e3H-2zQp9Benf1mi_-J4HVk")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", 8982017587))
 
-ADMIN_ID = 8982017587  # Senin Telegram ID'n
+OKX_API_KEY = os.environ.get("OKX_API_KEY")
+OKX_SECRET_KEY = os.environ.get("OKX_SECRET_KEY")
+OKX_PASSPHRASE = os.environ.get("OKX_PASSPHRASE")
